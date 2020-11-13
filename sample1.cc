f47085dec6dc163d81380475155e482b880cdf97
Jonas Wittbrodt <jonas.wittbrodt@desy.de>
2020-11-13 14:24:48 +0100

    format code
    also made GenericModel functions pure virtual

--- sample1.cc
+++ sample1.cc
@@ -1,48 +1,46 @@
-#include<iostream>
-#include"simplebounce.h"
+#include "simplebounce.h"
+#include <iostream>
 using namespace std;
 using namespace simplebounce;
 
-class MyModel : public GenericModel{
+class MyModel : public GenericModel {
   public:
-	MyModel(){
-		// number of scalar field(s)
-		setNphi(1);
-	}
-	// potential for scalar field(s)
-	double vpot (const double* phi) const{
-		return phi[0]*phi[0]/2. - phi[0]*phi[0]*phi[0]/3.;
-	}
-	// first derivative(s) of potential
-	void calcDvdphi(const double *phi, double *dvdphi) const{
-		dvdphi[0] = phi[0] - phi[0]*phi[0];
-	}
+    MyModel() {
+        // number of scalar field(s)
+        setNphi(1);
+    }
+    // potential for scalar field(s)
+    double vpot(const double *phi) const {
+        return phi[0] * phi[0] / 2. - phi[0] * phi[0] * phi[0] / 3.;
+    }
+    // first derivative(s) of potential
+    void calcDvdphi(const double *phi, double *dvdphi) const {
+        dvdphi[0] = phi[0] - phi[0] * phi[0];
+    }
 };
 
 int main() {
 
-	BounceCalculator bounce;
-	bounce.verboseOn(); // verbose mode
-	bounce.setRmax(1.); // phi(rmax) = phi(False vacuum)
-	bounce.setDimension(4); // number of space dimension
-	bounce.setN(100); // number of grid
-	MyModel model;
-	bounce.setModel(&model);
+    BounceCalculator bounce;
+    bounce.verboseOn();     // verbose mode
+    bounce.setRmax(1.);     // phi(rmax) = phi(False vacuum)
+    bounce.setDimension(4); // number of space dimension
+    bounce.setN(100);       // number of grid
+    MyModel model;
+    bounce.setModel(&model);
 
-	double phiTV[1] = {10.}; // a point at which V<0
-	double phiFV[1] = {0.}; // false vacuum
-	bounce.setVacuum(phiTV, phiFV);
+    double phiTV[1] = {10.}; // a point at which V<0
+    double phiFV[1] = {0.};  // false vacuum
+    bounce.setVacuum(phiTV, phiFV);
 
+    // calcualte the bounce solution
+    bounce.solve();
 
-	// calcualte the bounce solution
-	bounce.solve();
+    // show the results
+    bounce.printBounce();
 
-	// show the results
-	bounce.printBounce();
+    // show the Euclidean action
+    cout << "S_E = " << bounce.action() << endl;
 
-	// show the Euclidean action
-	cout << "S_E = " << bounce.action() << endl;
-
-	return 0;
+    return 0;
 }
-
